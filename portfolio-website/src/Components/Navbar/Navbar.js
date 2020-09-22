bd889b4849f504c37a00bb566d29f3ad810ccce7
Shan Memon <shankmemon@gmail.com>
2020-09-22 17:05:33 -0500

    started navbar

--- portfolio-website/src/Components/Navbar/Navbar.js
+++ portfolio-website/src/Components/Navbar/Navbar.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { IconContext } from 'react-icons/lib';
 import { Link } from 'react-router-dom';
+import { FaTimes, FaBars } from 'react-icons/fa';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const scrollTo = (position) =>
+    console.log('scrolling to position: ' + position);
+
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }} />
-      <div className='navbar'>
-        <Link to='/'>
-          <h2>ShanMemon.com</h2>
-        </Link>
-      </div>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='/' className='navbar-logo'>
+            <h2>ShanMemon.com</h2>
+          </Link>
+
+          <div className='menu-icon' onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <Link to='/resume' onClick={closeMobileMenu}>
+              <li className='nav-item'>Resume</li>
+            </Link>
+            <Link onClick={(closeMobileMenu, scrollTo(10))}>
+              <li className='nav-item'>Contact Me</li>
+            </Link>
+            <Link onClick={(closeMobileMenu, scrollTo(20))}>
+              <li className='nav-item'>Timeline</li>
+            </Link>
+          </ul>
+        </div>
+      </IconContext.Provider>
     </>
   );
 }
