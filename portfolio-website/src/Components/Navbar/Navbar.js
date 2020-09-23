2540e07b66bf3d6d2eae34d98f98d62e2a709c36
Shan Memon <shankmemon@gmail.com>
2020-09-23 01:32:03 -0500

    got navbar working

--- portfolio-website/src/Components/Navbar/Navbar.js
+++ portfolio-website/src/Components/Navbar/Navbar.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import { IconContext } from 'react-icons/lib';
-import { Link } from 'react-router-dom';
-import { FaTimes, FaBars } from 'react-icons/fa';
+import React, { useState } from 'react'
+import './Navbar.css'
+import { IconContext } from 'react-icons/lib'
+import { Link } from 'react-router-dom'
+import { FaTimes, FaBars } from 'react-icons/fa'
 
 function Navbar() {
-  const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false)
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
   const scrollTo = (position) =>
-    console.log('scrolling to position: ' + position);
+    console.log('scrolling to position: ' + position)
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
+      <IconContext.Provider value={{ color: 'black' }}>
         <div className='navbar'>
           <Link to='/' className='navbar-logo'>
             <h2>ShanMemon.com</h2>
@@ -24,20 +24,26 @@ function Navbar() {
             {click ? <FaTimes /> : <FaBars />}
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <Link to='/resume' className="nav-links" onClick={closeMobileMenu}>
+            <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
               <li className='nav-item'>Resume</li>
             </Link>
-            <Link className="nav-links" onClick={(closeMobileMenu, scrollTo(10))}>
+            <Link
+              className='nav-links'
+              onClick={(closeMobileMenu, scrollTo(10))}
+            >
               <li className='nav-item'>Contact Me</li>
             </Link>
-            <Link className="nav-links" onClick={(closeMobileMenu, scrollTo(20))}>
+            <Link
+              className='nav-links'
+              onClick={(closeMobileMenu, scrollTo(20))}
+            >
               <li className='nav-item'>Timeline</li>
             </Link>
           </ul>
         </div>
       </IconContext.Provider>
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
